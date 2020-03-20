310bd4a666b93388a02eac6a6900dcd3da7f46b2
Sam Ardis <ardis.productions@gmail.com>
2020-03-20 11:24:05 -0400

    Added file structure and coded index.js, starting on app.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+//imported necessary imports
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import {Reducer} from './reducers/Reducer';
+//create store
+const store = createStore(Reducer, applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<Provider store = {store}> 
+<App /> 
+</Provider>, document.getElementById("root"));
